f86bb406d8e2fa182c4111e89f8e8b852e148d66
archijaiswal5560 <jaiswalarchi55@gmail.com>
2025-11-05 02:13:51 +0530

    updates

--- src/components/About/Toolstack.js
+++ src/components/About/Toolstack.js
@@ -3,18 +3,13 @@ import { Col, Row } from "react-bootstrap";
 import {
   SiVisualstudiocode,
   SiPostman,
-  // SiSlack,
   SiVercel,
-  // SiMacos,
 } from "react-icons/si";
 import { FaAws } from "react-icons/fa";
 
 function Toolstack() {
   return (
     <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
-      {/* <Col xs={4} md={2} className="tech-icons">
-        <SiMacos />
-      </Col> */}
       <Col   md={2} className="tech-icons">
         <SiVisualstudiocode />
       </Col>
