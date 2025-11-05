9ec35d730ce1d98615a1c2357db49d3728549d4e
archijaiswal5560 <jaiswalarchi55@gmail.com>
2025-11-06 01:21:29 +0530

    updates

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -5,14 +5,14 @@ import Particle from "../Particle";
 import editor from "../../Assets/Projects/MMA.png";
 import shiptracker from "../../Assets/Projects/Shiptracker.png";
 import MMA from "../../Assets/Projects/MMA.png";
-
+import smartgroww from "../../Assets/Projects/smartgroww.png";
 function Projects() {
   return (
     <Container fluid className="project-section">
       <Particle />
       <Container>
         <h1 className="project-heading">
-          My Recent <strong className="purple">Works </strong>4r
+          My Recent <strong className="purple">Works </strong>
         </h1>
         <p style={{ color: "white" }}>
           Here are a few projects I've worked on recently.
@@ -24,7 +24,7 @@ function Projects() {
               isBlog={false}
               title="Ship Tracker"
               description="Engineered a real-time ship tracking system using MarineTraffic API, providing instant ETA updates for over 1,000 vessels daily.Implemented role-based access control, securing sensitive maritime data for multiple user roles.Optimized front-end (React.js) and back-end (Express.js) workflows, achieving 30% faster request processing on AWS."
-              ghLink="https://github.com/soumyajit4419/ShipTracker"
+              ghLink=""
               demoLink="https://track.shipsmithgroup.com/"
             />
           </Col>
@@ -35,19 +35,19 @@ function Projects() {
               isBlog={false}
               title="MMA"
               description="Developed a full-stack maritime course management platform supporting 500+ simultaneous enrollments with secure payment processing.Enhanced real-time seat tracking and dynamic discount logic, improving enrollment efficiency and customer satisfaction.My personal blog page build with Next.js and Tailwind Css which takes the content from makdown files and renders it using Next.js. Supports dark mode and easy to write blogs using markdown."
-              ghLink="https://github.com/soumyajit4419/Bits-0f-C0de"
-              demoLink="https://blogs.soumya-jit.tech/"
+              ghLink=""
+              demoLink="https://mmakolkata.marineorg.in/"
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={editor}
+              imgPath={smartgroww}
               isBlog={false}
               title="SmartGrow"
               description="AI-powered waterless indoor plantation system. Developed a modular indoor plantation system leveraging hydroporation-based watering and sensor automation (moisture, pH, light, temperature) for optimal plant growth. Integrated an AI-powered web application for real-time monitoring, plant health analysis, and growth recommendations."
-              ghLink="https://github.com/soumyajit4419/Editor.io"
-              demoLink="https://editor.soumya-jit.tech/"              
+              ghLink="https://github.com/archijaiswal5560/SmartGroww"
+              demoLink=""              
             />
           </Col>
         </Row>
